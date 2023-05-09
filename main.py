a675dfa9a5308f3fccfa74efa597ea7912dd5dee
Asadhjafri <51427124+Asadhjafri@users.noreply.github.com>
2023-05-09 16:23:43 +0000

    Add if else logic check for user input against computer. Add else for any invalid inputs.

--- main.py
+++ main.py
@@ -29,7 +29,16 @@ scissors = '''
 
 #Write your code below this line 👇
 
-user_choice = input(
-  "Lets play! Choose 0 for Rock, 1 for Paper or 2 Scissors to start. ")
+user_choice = int(input(
+  "Lets play! Choose 0 for Rock, 1 for Paper or 2 Scissors to start.\n "))
 
 computer_choice = random.randint(0, 2)
+
+print(f"Computer chose {computer_choice}")
+
+if user_choice == 0 and computer_choice == 1:
+  print("You win!")
+elif computer_choice > user_choice:
+  print("You lose!")
+else:
+  print("You typed an invalid number, you lose!")
