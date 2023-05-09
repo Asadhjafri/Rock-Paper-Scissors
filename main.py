4100c4c39665437f9feee61a106a5d6f7ac9fedc
Asadhjafri <51427124+Asadhjafri@users.noreply.github.com>
2023-05-09 16:33:27 +0000

    Finish logic for game. Create if conditional to check if user input exceeds 3. Gives invalid prompt.

--- main.py
+++ main.py
@@ -29,16 +29,23 @@ scissors = '''
 
 #Write your code below this line 👇
 
-user_choice = int(input(
-  "Lets play! Choose 0 for Rock, 1 for Paper or 2 Scissors to start.\n "))
+user_choice = int(
+  input(
+    "Lets play! Choose 0 for Rock, 1 for Paper or 2 Scissors to start.\n "))
 
 computer_choice = random.randint(0, 2)
 
 print(f"Computer chose {computer_choice}")
 
-if user_choice == 0 and computer_choice == 1:
+if user_choice >= 3 or user_choice < 0:
+  print("You typed an invalid number, you lose!")
+elif user_choice == 0 and computer_choice == 1:
   print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose!")
 elif computer_choice > user_choice:
   print("You lose!")
-else:
-  print("You typed an invalid number, you lose!")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It is a draw!")
